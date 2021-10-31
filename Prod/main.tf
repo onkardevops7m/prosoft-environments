47bdf1294077f87d6b3d120234c1339d844361c3
onkardevops7m <onkardevops7m@gmail.com>
2021-10-31 12:55:27 +0530

    add Prod env

---
+++ Prod/main.tf
@@ -0,0 +1,12 @@
+
+resource "aws_instance" "webserver" {
+  ami                    = "ami-013a129d325529d4d"
+  instance_type          = "t2.micro"
+  key_name               = "Windows"
+  subnet_id              = "subnet-efa85397"
+  count                  = 1
+  user_data              = file("script.sh")
+  tags = {
+    Name = "Prod-Webserver"
+  }
+}
---
+++ Prod/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-west-2"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "environment-state-devops"
+    key    = "Prod/Prod-state-file.tf"
+    region = "us-west-2"
+  }
+}
