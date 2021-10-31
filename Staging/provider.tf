99a9d1c4e16f345f1b2aa0cce6f4cf808df1ac3e
onkardevops7m <onkardevops7m@gmail.com>
2021-10-31 12:26:21 +0530

    update key

--- Staging/provider.tf
+++ Staging/provider.tf
@@ -3,13 +3,6 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "environment-state-devops"
-    key    = "QA/qa-state-file.tf"
-    region = "us-west-2"
-  }
-}
 
 terraform {
   backend "s3" {
