97bc57cf3bb7a8c6125a8e483ca28fe0150df7b7
onkardevops7m <onkardevops7m@gmail.com>
2021-10-31 11:34:26 +0530

    State sync logic

--- QA/provider.tf
+++ QA/provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
   version = "~> 2.0"
   region  = "us-west-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "environment-state-devops"
+    key    = "QA/qa-state-file.tf"
+    region = "us-west-2"
+  }
+}
--- Staging/provider.tf
+++ Staging/provider.tf
@@ -2,3 +2,19 @@ provider "aws" {
   version = "~> 2.0"
   region  = "us-west-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "environment-state-devops"
+    key    = "QA/qa-state-file.tf"
+    region = "us-west-2"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "environment-state-devops"
+    key    = "Staging/Staging-state-file.tf"
+    region = "us-west-2"
+  }
+}
