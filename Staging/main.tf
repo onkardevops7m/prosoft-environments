3589a60dfab8c3695c8bd98a034ce4e4e53f49c7
onkardevops7m <onkardevops7m@gmail.com>
2021-10-31 11:23:33 +0530

    add Staging environment

---
+++ Staging/main.tf
@@ -0,0 +1,12 @@
+
+resource "aws_instance" "webserver" {
+  ami                    = "ami-013a129d325529d4d"
+  instance_type          = "t2.micro"
+  key_name               = "Windows.pem"
+  subnet_id              = "subnet-efa85397"
+  count                  = 2
+  user_data              = file("script.sh")
+  tags = {
+    Name = "Staging-Webserver"
+  }
+}
--- Staging/script.sh
+++ Staging/script.sh
@@ -5,7 +5,7 @@ yum install httpd -y
 #yum install docker -y
 systemctl start httpd
 systemctl enable httpd
-echo "<h1> QA Webserver... </h1>" > /var/www/html/index.html
+echo "<h1> Staging Webserver... </h1>" > /var/www/html/index.html
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDP0T5RtsG4XZDZUXUJy49n+h8NeYa2k/O10bXjmsueF2GVVUsr8TdKqUV4BuhOLbRupMUiVSw3RgXsEJbnLsv+hrefSIHF4wRA/QwmXUeCaR0xC0orESlGLhAswxS1LKLK0zbgfOul2HktmC73iIywcpK9TZxsHtTegxBX6Gk4NfaoXkgzKMZkiw5Yp0GvANGXvJgTZeRfaPgy5Wzbtj/MNR3ihfJAwV98blVX9ey+ABb0T4Xa6YERCOoNgRefltU8rnPujQv0TH+9gpQFn6wW9CpvumM1f7ki9fDdZoFWgiDrNlVRpR8Y0qoWzTU84WuHPhBMHterEzBs8YrDY/fx jenkins@ip-172-31-17-87.us-west-2.compute.internal" >> /root/.ssh/authorized_keys
 
